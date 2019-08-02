4b75dfd0d352f768a233dd57cb4c8f309deae3be
Radioant <radioant@163.com>
2019-08-02 21:58:56 +0800

    1st

--- 回数.py
+++ 回数.py
@@ -4,7 +4,7 @@
 digits=[]
 for i in range(10,1000):
     temp=str(i)             #数字转字符串
-    digit=temp[::-1]        #字符颠倒次序
+    digit=temp[::-1]        #字符颠倒次序,例１２３　变成　３２１
     if temp==digit:
         digits.append(int(temp))
 
